56a6523018df9b681a6d733243ca92a4479ab838
Uris <urisnicolas@gmail.com>
2019-06-12 14:05:41 -0400

    creacion de un objeto utilizando notaciones literales

---
+++ js/object-literal2.js
@@ -0,0 +1,16 @@
+var hotel = {
+    name: 'Park',
+    rooms: 120,
+    booked: 77,
+    checkAvailability: function () {
+        return  this.rooms - this.booked;
+
+    }
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
+
