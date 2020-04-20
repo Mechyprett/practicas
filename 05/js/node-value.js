0801e7ce72aa50a16b7f48d8c13a9b801e2a7e5a
Uris <urisnicolas@gmail.com>
2020-04-19 18:39:10 -0800

    Accessing & Changing A text Node

---
+++ 05/js/node-value.js
@@ -0,0 +1,8 @@
+var itemTwo = document.getElementById('two');   //Get second list item
+
+var elText = itemTwo.firstChild.nodeValue; //Get its text content
+
+elText = elText.replace('pine nut', 'kale');  //Change pine nuts to kale
+
+itemTwo.firstChild.nodeValue = elText;  //Update the list item
+
