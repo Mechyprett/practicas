03f2476787296a5eb2191a17b4a99921b7280bb4
Uris <urisnicolas@gmail.com>
2020-04-19 19:24:44 -0800

    Update text & markup

--- 05/js/inner-html.js
+++ 05/js/inner-html.js
@@ -1,8 +1,8 @@
-//Store the first list item in a variable
+// Store the first list item in a variable
 var firstItem = document.getElementById('one');
 
-//Get the content of the first list item
+// Get the content of the first list item
 var itemContent = firstItem.innerHTML;
 
-//Update the content of the first list item so it is a link
-firstItem.innerHTML = '<a href=/"http://example.org\">' + itemContent + '</a>';
+// Update the content of the first list item so it is a link
+firstItem.innerHTML = '<a href=\"http://example.org\">' + itemContent + '</a>';
